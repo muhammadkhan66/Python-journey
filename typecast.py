7a5980a5cd30768ef1c1a2c407c9677b21a7c9b5
muhammadkhan66 <mabdulrehmankhan31@gmail.com>
2025-08-16 02:47:34 -0700

    Add files via upload

---
+++ typecast.py
@@ -0,0 +1,42 @@
+# First Question
+
+# a = input("Enter a number: ")
+
+# print("This is the original Data type:", a)
+# print(type(a))
+
+# b = int(a)
+# print("This  is the converted Data type:", b)
+# print(type(b))
+
+# Second Question
+
+# x = 7.9
+# # y = int(x)
+# print(x)
+
+# Third Question
+
+# x = "123.45"
+
+# y = float(x)
+# print(y)
+# print(type(y))
+
+# z = int(y)
+# print(z)
+# print(type(z))
+
+# Fourth Question
+
+# a = "10"
+# b = 5
+# print(int(a) + b)
+
+data = ["10", "20.5", 30, "40"]
+data1=["]
+
+for i in data:
+    
+
+
