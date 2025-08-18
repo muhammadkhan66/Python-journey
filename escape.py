9d78ad9e15f11d4c4e9ebc83fcb70e19aaad7924
muhammadkhan66 <mabdulrehmankhan31@gmail.com>
2025-08-17 23:43:54 -0700

    Add files via upload

---
+++ escape.py
@@ -0,0 +1,25 @@
+# Question # 1
+print("Item1\tItem2\tItem3\nPrice1\tPrice2\tPrice3")
+
+# Question # 2
+
+print("C:\\new_folder\\python\\scripts")
+
+print("C:\\new_folder\python\scripts")
+
+# Question # 3
+
+print("Hello\nWorld\t2025\\")
+
+# Question # 4
+
+
+print("He said, \"Python's awesome!")
+
+# Question # 5
+
+
+print("Good\bye")
+
+
+
