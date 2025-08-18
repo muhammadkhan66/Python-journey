e769a43528088c7fe50554f1e2395196608567d4
muhammadkhan66 <mabdulrehmankhan31@gmail.com>
2025-08-17 23:49:11 -0700

    escape.py

--- escape.py
+++ escape.py
@@ -1,25 +1,42 @@
 # Question # 1
+# Write a Python string that prints exactly the following output using escape sequences:
+Item1    Item2    Item3
+Price1   Price2   Price3
+
 print("Item1\tItem2\tItem3\nPrice1\tPrice2\tPrice3")
 
 # Question # 2
+#You want to display the following file path in Python without breaking it into multiple lines:
+# C:\new_folder\python\scripts
+# Write two different ways to achieve this using escape sequences (hint: normal escape sequences vs raw strings).
+
 
 print("C:\\new_folder\\python\\scripts")
 
 print("C:\\new_folder\python\scripts")
 
 # Question # 3
-
+# What will be the output of the following code snippet, and why?
 print("Hello\nWorld\t2025\\")
+# Output: 
+# Hello
+# World   2025\
 
 # Question # 4
+# Write a Python statement using escape sequences that prints a single line like this:
+# He said, "Python's awesome!"
+
 
 
 print("He said, \"Python's awesome!")
 
 # Question # 5
-
+# Suppose you want to include a backspace effect in your output.
+# What will be printed by this code and why?
 
 print("Good\bye")
+#  Output: 
+# Goobye 
 
 
 
