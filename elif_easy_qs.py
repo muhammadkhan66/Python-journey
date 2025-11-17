8e12653ac8fc19a2a2592674d11d207658879051
muhammadkhan66 <mabdulrehmankhan31@gmail.com>
2025-11-17 00:51:17 -0800

    Add files via upload

---
+++ elif_easy_qs.py
@@ -0,0 +1,91 @@
+# # # Question 1
+number = int(input("Enter the  number\n"))
+
+if  number > 0:
+    print("Number is positive.")
+else:
+    print("Number is negative.")
+
+# # # Question 2
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# # # Question 3
+
+num =  int(input("Enter the  number\n"))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# # # Question 4
+
+num1 = int(input("Enter number 1\n"))
+num2 = int(input("Enter number 2\n"))
+
+if num1 > num2:
+    print("Number 1 is greater.")
+else:
+    print("Number 2 is greater.")
+
+# # # Question 5
+
+password = input("Enter the password\n")
+
+if password == "admin123":
+    print("Access granted")
+else:
+    print("Access denied")
+
+# # # Question 6
+
+temperature =  int(input("Enter the temperature\n"))
+
+if temperature > 30:
+    print("It's hot!")
+else:
+    print("Weather is  normal.")
+
+
+# # # Question 7
+
+character =  input("Enter the  character\n")
+
+if character in "a,e,i,o,u":
+    print("Vowel")
+else:
+    print("Consonant")
+
+# # # Question 8
+
+marks = int(input("Enter the marks 0-100\n"))
+
+if marks >= 50:
+    print("Pass")
+else:
+    print("Fail")
+
+# # # Question 9
+
+numberr = int(input("Enter the number\n"))
+
+if numberr % 5 ==0:
+    print("Divisible by 5")
+else:
+    print("Not divisible by 5")
+
+# # # Question 10
+
+username = input('Enter the username\n')
+
+if username == "Muhammad":
+    print("Welcome!")
+else:
+    print("Unknown user.")
+
