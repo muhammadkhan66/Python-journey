23f1fae458a5e9b759d21313aa850414c1223746
muhammadkhan66 <mabdulrehmankhan31@gmail.com>
2026-01-21 11:44:56 -0800

    Add files via upload

---
+++ Python_Projects/Python_Projects/factorial_counter.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n ==0 or n ==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
+num = int(input("Enter the number:  "))
+
+if num < 0:
+    print("Factorial number is not possible for negative numbers")
+else:
+    result = factorial(num)
+print(f"Factorial of {num} is {result}")
+    
+
